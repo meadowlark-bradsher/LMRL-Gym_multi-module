64306af06a9e7b7a9250967a6b0c21b8f82af35c
meadowlark-bradsher <meadowlarkb@gmail.com>
2025-05-04 14:15:26 -0700

    dep management

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read_requirements_file(filename):
 
 setup(
     name='LLM_RL',
-    version='1.0.0',
+    version='1.0.1',
     description='Implementations of LLM Reinforcement Learning algorithms in JAX.',
     url='https://github.com/Sea-Snell/LLM_RL',
     author='Charlie Snell',
